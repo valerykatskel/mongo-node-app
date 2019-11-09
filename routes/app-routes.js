f6022cb59f6e0984d7b86ac2ccec1356f0eb0d2a
Valery Katskel <ne.vse.srazu@gmail.com>
2019-11-09 21:50:59 +0300

    active menu item highlight

--- routes/app-routes.js
+++ routes/app-routes.js
@@ -3,13 +3,15 @@ const router = Router()
 
 router.get('/', (req, res) => {
     res.render('pages/index', {
-        title: 'Todos list'
+        title: 'Todos list',
+        isIndex: true,
     })
 })
 
 router.get('/create', (req, res) => {
     res.render('pages/create', {
-        title: 'Create a new todo'
+        title: 'Create a new todo',
+        isCreate: true,
     })
 })
 
